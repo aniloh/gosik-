27eea05b7d607bf91225d19912c32d2809b3ce88
aniloh <83836356+aniloh@users.noreply.github.com>
2022-10-03 13:27:23 +0300

    Add files via upload

---
+++ Госпортал/authorization.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<title>ГосПортал</title>
+	<link rel="stylesheet" href="style.css" type="text/css">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,300" type="text/css">
+</head>
+<body link="white" vlink="#cecece" alink="#ff0000">
+    <header>
+         
+        <div id="top-header">
+            <img src="logoo.png" alt="Girl in a jacket" >
+            
+            <nav>
+                <ul>
+                    <li class="active1"><a href="#">Главная</a></li>
+                    <li class="active3"><a href="#">Личный кабинет</a></li>
+                </ul>
+            </nav>
+        </div>   
+     
+        </div>
+    </header>
+    <div id="block">
+    <p class="auto1"> Регистрация </p>  
+    
+    <p class="auto2"> ФИО </p>
+    <p class="auto3"> Логин </p>
+    <p class="auto4"> Emai </p>
+    <p class="auto5"> ПаролЬ</p>
+    <p class="auto6"> Повтор пароля </p>
+    <p class="auto7"> Даю согласие на обработку персональных данных </p>
+    <p class="auto"> Регистрация </p>
+    <p class="auto"> Нет аккаунта? Авторизоваться </p>
+    </div>
+
+    </div>
+</body>
+</html>
